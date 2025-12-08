f19b43c33ed52b88f7261babd0f9b859d1d539b6
Dimitrije <dstojanovga@gmail.com>
2025-12-08 16:48:54 +0100

    Added OpenApi documentation: schema name and description added in DTO classes

--- Section 2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ Section 2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,19 +1,33 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
+    @Schema(
+            description = "Account number of Eazy Bank account", example = "1234567890"
+    )
     @NotEmpty(message = "Account number can not be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of Eazy Bank account", example = "Savings"
+    )
     @NotEmpty(message = "Account type can not me null or empty")
     private String accountType;
 
+    @Schema(
+            description = "Eazy Bank branch address", example = "Novi Sad, Ulica 123"
+    )
     @NotEmpty(message = "Branch address can not me null or empty")
     private String branchAddress;
 
