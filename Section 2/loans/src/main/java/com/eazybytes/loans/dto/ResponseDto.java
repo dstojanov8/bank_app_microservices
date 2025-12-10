4bbf904d6da7d269a0314164acb7cde0c825bd74
Dimitrije <dstojanovga@gmail.com>
2025-12-10 18:42:20 +0100

    Created DTO classes

---
+++ Section 2/loans/src/main/java/com/eazybytes/loans/dto/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.eazybytes.loans.dto;
+
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String statusCode;
+
+    private String statusMsg;
+}
