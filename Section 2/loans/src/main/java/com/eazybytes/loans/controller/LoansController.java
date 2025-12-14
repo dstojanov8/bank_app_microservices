096f9bfb60c9e3bb30c07d798728f7f74de30985
Dimitrije <dstojanovga@gmail.com>
2025-12-14 11:16:30 +0100

    Create new Loan functionality added (Controller + Service + Repository)

--- Section 2/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ Section 2/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -1,14 +1,32 @@
 package com.eazybytes.loans.controller;
 
+import com.eazybytes.loans.constants.LoansConstants;
+import com.eazybytes.loans.dto.ResponseDto;
+import com.eazybytes.loans.service.ILoansService;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/loans")
+@RequestMapping(path = "/loans", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+@Validated
 public class LoansController {
 
-    @PostMapping(path = "/create")
-    public ResponseEntity<> createLoan()
+    private final ILoansService loansService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createLoan(@RequestParam
+                                                  @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                  String mobileNumber) {
+        loansService.createLoan(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
+    }
+
 }
